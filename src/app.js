acb4d24d7e9a0e9551a822fbb5e0611b9601b558
unknown <guilhermeagutoli@gmail.com>
2019-03-23 11:30:11 -0300

    commit api rest node js 0.0.1

---
+++ src/app.js
@@ -0,0 +1,18 @@
+'use strict';
+const express = require('express');
+const bodyParser = require('body-parser');
+
+const app = express();
+const router = express.Router();
+
+//carregar rotas
+const indexRoute = require('./routes/indexRoute');
+const productRoute = require('./routes/productRoute');
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+
+app.use('/', indexRoute);
+app.use('/products', productRoute);
+
+module.exports = app;
---
+++ src/routes/indexRoute.js
@@ -0,0 +1,12 @@
+'use strict';
+const express = require('express');
+const router = express.Router();
+
+router.get('/', (req, res, next) => {
+    res.status(200).send({
+        title: "Node api",
+        version: "0.0.1"
+    });
+});
+
+module.exports = router;
